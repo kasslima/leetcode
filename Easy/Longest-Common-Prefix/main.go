7b2a3b11c5da17cf8de114a8fa7ad4177081600c
Lucas Ribeiro <lucasribeiro292004@hotmail.com>
2026-03-03 23:00:29 -0300

    Refactor: using two aligned loops, now 125/126 test cases pass

--- Easy/Longest-Common-Prefix/main.go
+++ Easy/Longest-Common-Prefix/main.go
@@ -3,52 +3,77 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println(longestCommonPrefix([]string{"flower","flower","flower", "flower"}))
+	//fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+	//fmt.Println(longestCommonPrefix([]string{"ab", "a"}))
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 }
 
 
 func longestCommonPrefix(strs []string) string {
-    
-    if len(strs) == 1{
-        return strs[0]
-    }
 
-    j := 1
-    
-    prefix := strs[0][:j]
+	if len(strs) == 1 {
+		return strs[0]
+	}
 
-    for i := range strs {
+	strs = equalizarPeloMenor(strs)
 
-        if i >= len(strs) || j > len(strs[i]){
-            return prefix
-        }
+	prefix := strs[0]
 
-        if prefix != strs[1][:j] && j == 1 {
-            return ""
-        }
 
-        if prefix != strs[i][:j] {
-            return prefix[:j-1]
-        }
+	j:= 0
 
-        j++
+	var k int
 
-        prefix = strs[0][:j]
+	temp:= ""
 
-        if len(strs[i]) >= j{
-            if len(strs[i+1]) < j{
-                return prefix[:j-1]
-            } 
+	for i := range strs{
 
-            if prefix != strs[i+1][:j]{
-                return prefix[:j-1]
-            }
+		k = 0
 
-        }
-        
-        
-    }
+		for j = 0; j <= len(strs[i]); j++{
+			word := strs[i]
 
-    return "nil"
+			if k == len(word) {
+				prefix = word[:k]
+				break
+			}
 
+			if word[:j+1] != prefix[:j+1] {
+				if j == 0 {
+					return ""
+				}
+				return word[:k]
+			}
+			k++
+			
+		}
+
+		if temp != prefix[:k] {
+			temp = prefix[:k]
+		}
+		
+	}
+
+	return prefix[:k]
+
+}
+
+func equalizarPeloMenor(strs []string) []string {
+	if len(strs) == 0 {
+		return strs
+	}
+
+	menor := len(strs[0])
+	for _, s := range strs {
+		if len(s) < menor {
+			menor = len(s)
+		}
+	}
+
+	resultado := make([]string, len(strs))
+	for i, s := range strs {
+		resultado[i] = s[:menor]
+	}
+
+	return resultado
 }
